8e6bcb2a874616d1adf50b3cd78f76b89b8bbf00
GrecoSl <redesprgreco@gmail.com>
2026-08-21 15:53:23 -0600

    se repara la imagen del login

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <head>
+        <meta name="color-scheme" content="light" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
